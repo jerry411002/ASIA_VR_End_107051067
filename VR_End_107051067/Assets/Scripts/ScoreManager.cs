7094a90586c857bb42295c92d6d59bffaca3dbcd
jerry411002 <71679892+jerry411002@users.noreply.github.com>
2021-01-11 13:16:47 +0800

    1

--- VR_End_107051067/Assets/Scripts/ScoreManager.cs
+++ VR_End_107051067/Assets/Scripts/ScoreManager.cs
@@ -8,7 +8,7 @@ public class ScoreManager : MonoBehaviour
     public Text textScore;
     [Header("分數")]
     public int score;
-    [Header("投進的分數")]
+    [Header("投僅的分數")]
     public int scoreIn = 2;
     [Header("進球音效")]
     public AudioClip soundIn;
@@ -18,36 +18,35 @@ public class ScoreManager : MonoBehaviour
     private void Awake()
     {
         aud = GetComponent<AudioSource>();
-    }
+  }
 
     private void OnTriggerEnter(Collider other)
     {
-       
-        if (other.tag == "Bomb"&& other.transform.position .y >2.5f )
+        if (other.tag=="Bomb"&& other.transform.position.y>2.5f)
         {
             AddScore();
         }
-        
-        if (other.transform.root.name=="Player")
+        if (other.transform .root.name =="Player")
         {
-            scoreIn =5;
+            scoreIn = 5;
         }
-    }
-    
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.transform.root .name=="Player")
-        {
-           
-            scoreIn = 2;
-        }  
     }
 
-    private  void  AddScore()
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform .root.name=="Player")
         {
-            score += scoreIn;                  
-            textScore.text = "分數" + score;  
-            aud.PlayOneShot(soundIn, Random.Range(1f, 2f)); //音效來源.撥放一次(音效片段,音量)
+            scoreIn = 2;
         }
-   
+    }
+
+    private void AddScore()
+    {
+        score += scoreIn;
+        textScore.text = "分數" + score;
+
+        aud.PlayOneShot(soundIn, Random.Range(1f, 3f));
+
+    }
+
 }
